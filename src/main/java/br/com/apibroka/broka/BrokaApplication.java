281abd1ed7b3ade3b4ef6b63e94b05213bce1c56
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-07-26 22:16:55 -0400

    feat: inicia o projeto

---
+++ src/main/java/br/com/apibroka/broka/BrokaApplication.java
@@ -0,0 +1,13 @@
+package br.com.apibroka.broka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/apibroka/broka/BrokaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apibroka.broka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
