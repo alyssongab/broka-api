fd7f160fde1adcb382eb13407357769f26e3947c
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-07-28 03:19:50 -0400

    feat: cria servico de gerar, validar e expirar token

---
+++ src/main/java/br/com/apibroka/broka/service/TokenService.java
@@ -0,0 +1,52 @@
+package br.com.apibroka.broka.service;
+
+import br.com.apibroka.broka.model.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class TokenService {
+
+    @Value("${api.security.jwt.secret}")
+    private String secret;
+
+    private String gerarToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("api-broka")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(gerarDataExpiracao())
+                    .sign(algorithm);
+            return token;
+        }
+        catch(JWTCreationException e){
+            throw new RuntimeException("Erro ao gerar token JWT", e);
+        }
+    }
+
+    public String validarToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("api-broka")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }
+        catch(JWTVerificationException e){
+            return "";
+        }
+    }
+
+    public Instant gerarDataExpiracao(){
+        // expirar o token em 2h
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-04:00"));
+    }
+}
