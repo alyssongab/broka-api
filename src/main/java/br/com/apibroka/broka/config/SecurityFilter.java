e2ad06955cee8714e4adb625f49d3ced8fd8602e
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-07-28 03:54:21 -0400

    feat: cria o filtro de seguranca para interceptar requisicoes

---
+++ src/main/java/br/com/apibroka/broka/config/SecurityFilter.java
@@ -0,0 +1,50 @@
+package br.com.apibroka.broka.config;
+
+import br.com.apibroka.broka.model.User;
+import br.com.apibroka.broka.repository.UserRepository;
+import br.com.apibroka.broka.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recuperarToken(request);
+        if(token != null){
+            var email = tokenService.validarToken(token);
+            Optional<User> userOpt = userRepository.findByEmail(email);
+
+            if(userOpt.isPresent()){
+                var user = userOpt.get();
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
