022df63ae7008659c66fcfa792e752a53c77d436
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-07-30 02:15:12 -0400

    feat: cria endpoint para cliente criar pedido

---
+++ src/main/java/br/com/apibroka/broka/controller/OrderController.java
@@ -0,0 +1,29 @@
+package br.com.apibroka.broka.controller;
+
+import br.com.apibroka.broka.dto.order.OrderRequestDTO;
+import br.com.apibroka.broka.model.User;
+import br.com.apibroka.broka.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    @PreAuthorize("hasRole('CLIENTE')")
+    public ResponseEntity<String> placeOrder(@RequestBody OrderRequestDTO dto, @AuthenticationPrincipal User client){
+        var createdOrder = orderService.createOrder(dto, client);
+        return ResponseEntity.status(201).body("Pedido #" + createdOrder.getId() + " realizado com sucesso.");
+    }
+}
